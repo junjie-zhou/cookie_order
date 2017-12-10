f162b0d1c0f580d10bc30ab7989a0081edd24fef
YaoRui <30330466+goSummmer@users.noreply.github.com>
2017-12-10 21:05:38 +0800

    恢复OrderMaster--->DAO

--- src/main/java/com/cookie/dao/OrderMasterMapper.java
+++ src/main/java/com/cookie/dao/OrderMasterMapper.java
@@ -3,7 +3,6 @@ package com.cookie.dao;
 import com.cookie.dto.OrderMasterDTO;
 import com.cookie.pojo.OrderMaster;
 
-<<<<<<< Updated upstream
 import java.util.List;
 
 /**
@@ -11,32 +10,13 @@ import java.util.List;
  */
 public interface OrderMasterMapper {
 
-    boolean  addOrderMaster(OrderMaster orderMaster);
+    boolean addOrderMaster(OrderMaster orderMaster);
 
-    boolean  updateOrderMaster(OrderMaster orderMaster);
+    boolean updateOrderMaster(OrderMaster orderMaster);
 
     OrderMasterDTO getOrderMasterByOrderId(OrderMaster orderMaster);
 
     List<OrderMasterDTO> getOrderMasterList(OrderMaster orderMaster);
 
     boolean deleteOrderMasterByParam(OrderMaster orderMaster);
-=======
-/**
- * Created by asus on 2017/12/10.
- */
-public interface OrderMasterMapper {
-
-    OrderMasterDTO findByOrderId(OrderMaster orderMaster);
-
-    OrderMasterDTO findByBuyerOpenId(OrderMaster orderMaster);
-
-    boolean insert(OrderMaster orderMaster);
-
-    boolean updateByOrderId(OrderMaster orderMaster);
-
-    boolean delectByOrderId(OrderMaster orderMaster);
-
-    boolean deleteByBuyerOpenId(OrderMaster orderMaster);
-
->>>>>>> Stashed changes
 }
--- src/main/test/com/cookie/dao/test/OrderMasterDAOTest.java
+++ src/main/test/com/cookie/dao/test/OrderMasterDAOTest.java
@@ -1,15 +1,9 @@
 package com.cookie.dao.test;
 
 import com.cookie.dao.OrderMasterMapper;
-<<<<<<< Updated upstream
 import com.cookie.dto.OrderMasterDTO;
 import com.cookie.pojo.OrderMaster;
-=======
-import com.cookie.dao.ProductCategoryMapper;
-import com.cookie.dto.ProductCategoryDTO;
-import com.cookie.pojo.OrderMaster;
-import com.cookie.pojo.ProductCategory;
->>>>>>> Stashed changes
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,18 +11,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-<<<<<<< Updated upstream
+
 import java.math.BigDecimal;
 
 /**
  * Created by zjj on 2017/12/10.
  */
-=======
 
-import java.math.BigDecimal;
-import java.util.Date;
-
->>>>>>> Stashed changes
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -38,7 +27,6 @@ public class OrderMasterDAOTest {
     private OrderMasterMapper orderMasterMapper;
 
     @Test
-<<<<<<< Updated upstream
     public void addOrderMaster(){
         OrderMaster pojo=new OrderMaster("CK4378343","测试","测试","测试","测测",new BigDecimal(687.56),1,1);
         boolean result =orderMasterMapper.addOrderMaster(pojo);
@@ -52,22 +40,5 @@ public class OrderMasterDAOTest {
         Assert.assertNotNull(dto);
         System.out.println(dto.toString());
     }
-=======
-    public void testInsert(){
-        OrderMaster orderMaster=new OrderMaster("100001","zhangsan","18888888888","北京朝阳区","18888888888",
-                new BigDecimal("1200.6"),0,0,new Date(),new Date());
-        boolean insert = orderMasterMapper.insert(orderMaster);
-        System.out.println(insert);
-    }
 
-    @Test
-    public void testUpdateByOrderId(){
-        OrderMaster orderMaster=new OrderMaster("100001","zhangsan","18888888888","北京朝阳区","18888888888",
-                new BigDecimal("1200.6"),0,1,new Date(),new Date());
-        boolean insert = orderMasterMapper.updateByOrderId(orderMaster);
-        System.out.println(insert);
-    }
-
-
->>>>>>> Stashed changes
 }
