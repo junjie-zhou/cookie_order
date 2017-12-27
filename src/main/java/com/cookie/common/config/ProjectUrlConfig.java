c457930fcb18644b393475c719d132dc806194d0
junjie-zhou <zjjhero@126.com>
2017-12-27 23:14:38 +0800

    添加路径

--- src/main/java/com/cookie/common/config/ProjectUrlConfig.java
+++ src/main/java/com/cookie/common/config/ProjectUrlConfig.java
@@ -3,9 +3,11 @@ package com.cookie.common.config;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 
 @Data
+@PropertySource(value = "classpath:application.yml")
 @ConfigurationProperties(prefix = "projectUrl")
 @Configuration
 public class ProjectUrlConfig {
